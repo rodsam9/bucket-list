cba5e169644ade366b51e8e9058ab1ae90436f4b
rodsam9 <sr_odriguez9@yahoo.com>
2022-05-21 12:18:43 -0600

    set up mongodb connection

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const connect = require('./db/connect');
+
+
+app
+    .use(bodyParser.json())
+    .use('/', require('./routes'))
+    .use((req, res, next) => {
+        res.setHeader('Acces-Control-Allow-Origin', '*');
+        next();
+    });
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
